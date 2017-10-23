c8c4ae227c0a94a5ee6be560cc0e700aa552d226
Penguinboy95 <dultsevalexey95@mail.ru>
2017-10-24 00:24:28 +0500

    graphs need to be redisigned

---
+++ assets/js/detailCharts.js
@@ -0,0 +1,31 @@
+var now = new Date();
+
+var ctx = document.getElementById("statChart").getContext('2d');
+var myChart = new Chart(ctx, {
+    type: 'line',
+    data: 
+    {
+        labels: ["Date1", "Date2", "Date3", "Date4", "Date5", "Date6"],
+        datasets: [
+            {
+                label: 'Продажи',
+                fill: false,
+                lineTension: 0.1,
+                pointBackgroundColor: 'black',
+                data: [7, 5, 3, 5, 2, 8],
+                borderWidth: 1,
+                borderColor: 'blue'
+            }
+        ]
+    },
+options: {
+        scales: {
+            yAxes: [{
+                scaleLabel: {
+                    display: true,
+                    labelString: 'Продажи в количестве'
+                }
+            }]
+    }     
+}
+});
